52f922f1fa92a3339a1e0c7a9baac6906943fc16
thedarkarchitect <romanreynolds240@gmail.com>
2024-05-31 02:31:10 +0300

    wishlist functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -18,81 +18,9 @@ import AuthProvider from "./AuthProvider";
 import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
-	const [products, setProducts] = useState([]);
-	const [wishlist, setWishlist] = useState([]);
-
-	useEffect(() => {
-		const fetchProducts = async () => {
-			try {
-				const res = await fetch("https://petco.onrender.com/api/v1/products/");
-				const data = await res.json();
-				setProducts(data.products);
-			} catch (error) {
-				console.log("Error fetching data", error);
-			}
-		};
-
-		fetchProducts();
-	}, []);
-
-	// const toggleWishlist = (id) => {
-	// 	setWishlist((prevWishlist) =>
-	// 		prevWishlist.includes(id)
-	// 			? prevWishlist.filter((productId) => productId !== id)
-	// 			: [...prevWishlist, id]
-	// 	);
-	// };
-
-	// const router = createBrowserRouter(
-	// 	createRoutesFromElements(
-	// 		<Route>
-	// 			{/* Auth Routes */}
-	// 			<Route path="/auth">
-	// 				<Route path="/auth/SignUp" element={<SignUp />} />
-	// 				<Route path="/auth/Login" element={<Login />} />
-	// 			</Route>
-
-	// 			{/* MainLayout */}
-	// 			<Route path="/" element={<MainLayout />}>
-	// 				<Route index element={<Home />} />
-
-	// 				{/* products routes */}
-	// 				<Route path="/products">
-	// 					<Route path="/products/createProduct" element={<ProductUpload />} />
-	// 					<Route path="/products/:id" element={<ProductDetails />} />
-	// 					<Route path="/products/shop" element={<Shop products={products} wishlist={wishlist} toggleWishlist={toggleWishlist} />} />
-	// 				</Route>
-
-	// 				{/* posts routes */}
-	// 				<Route path="/posts">
-	// 					<Route path="/posts/createPost" element={<PostUpload />} />
-	// 					<Route path="/posts/blog" element={<Posts />} />
-	// 					<Route path="/posts/blog/:id" element={<PostDetails />} />
-	// 				</Route>
-
-	// 				{/* service routes */}
-	// 				<Route path="/service">
-	// 					<Route index element={<Service />} />
-	// 				</Route>
-
-	// 				{/* wishlist routes */}
-	// 				<Route path="/wishlist">
-	// 					<Route index element={<Wishlist products={products} wishlist={wishlist} toggleWishlist={toggleWishlist} />} />
-	// 				</Route>
-
-	// 				{/* about routes */}
-	// 				<Route path="/about" element={<AboutUs />} />
-	// 			</Route>
-	// 		</Route>
-	// 	)
-	// );
-
+	
 	return (
-		// 	<RouterProvider router={router}>
-		// 	<AuthProvider>
-		// 		<RouterProvider router={router} />
-		// 	</AuthProvider>
-		// </RouterProvider>
+		
 		<Router>
 			<AuthProvider>
 				<Routes>
@@ -117,11 +45,7 @@ const App = () => {
 							<Route
 								path="/products/shop"
 								element={
-									<Shop
-										// products={products}
-										// wishlist={wishlist}
-										// toggleWishlist={toggleWishlist}
-									/>
+									<Shop />
 								}
 							/>
 						</Route>
@@ -139,7 +63,7 @@ const App = () => {
 						</Route>
 
 						{/* wishlist routes */}
-						<Route path="/wishlist" element={<PrivateRoute />}>
+						<Route path="/wishlist">
 							<Route
 								index
 								element={
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WishlistCard from "../components/WishlistCard";
+import { jwtDecode } from "jwt-decode";
 
 const Wishlist = () => {
-    // const wishlistProducts = products.filter(product => wishlist.includes(product.id))
+	const isLogged = localStorage.getItem("token");
+	const [wishlist, setWishlist] = useState([]);
+    const [ownerId, setOwnerId] = useState(0);
+
+	useEffect(() => {
+		if (isLogged) {
+			const userId = jwtDecode(isLogged);
+			console.log(userId.userid);
+            setOwnerId(userId.userid)
+
+			const fetchWishlist = async () => {
+				try {
+					const response = await fetch(
+						`https://petco.onrender.com/api/v1/wishlist/get-user-wishlist/${userId.userid}`
+					);
+					const data = await response.json();
+					console.log(data);
+					setWishlist(data.wishList || []);
+				} catch (error) {
+					console.log(error);
+				}
+			};
+
+			fetchWishlist();
+		}
+	}, [isLogged]);
+
 	return (
-        <div>Wishlist</div>
-    );
+		<div className="grid gap-4 sm:grid-cols-1 sm:justify-center sm:p-12 md:grid-cols-2 lg:grid-row-3 xl:grid-row-4">
+			{wishlist.map((items) => (
+				<div key={items.id} className="">
+					<WishlistCard
+						id={items.id}
+						productId={items.productId}
+                        ownerId={ownerId}
+					/>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Wishlist;
